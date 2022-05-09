c3be359beaf58c20c604caeaeae6050cb21caf3f
marcus <buba.sergipe@gmail.com>
2022-05-09 16:57:08 -0300

    Final adjusts

--- app/components/Dropdown-Item.tsx
+++ app/components/Dropdown-Item.tsx
@@ -1,20 +1,6 @@
 import type { Pokemon } from "@prisma/client";
 import { Link } from "@remix-run/react";
 
-const cardColors = {
-  rock: "rgb(148, 81, 81)",
-  ghost: "rgb(247, 247, 247)",
-  electric: "rgb(255, 255, 161)",
-  bug: "#f6d6a7",
-  poison: "#e0a7f6",
-  normal: "#f4f4f4",
-  fairy: "rgba(255, 192, 203, 0.863)",
-  fire: "#fbe3df",
-  grass: "#e2f9e1",
-  water: "#e0f1fd",
-  ground: "#C2B232",
-};
-
 interface Props {
   captured?: Pokemon;
 }
