a366fa425168d7cfae8051956ca761dc5d0fe9b2
marcus <buba.sergipe@gmail.com>
2022-05-13 09:43:20 -0300

    Adjusts on remix config

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,8 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  serverBuildPath: "build/index.js",
+  browserBuildDirectory: "public/build",
   publicPath: "/build/",
+  serverBuildDirectory: "api/_build",
 };
